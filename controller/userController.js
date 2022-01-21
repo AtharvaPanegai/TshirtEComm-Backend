9da4a5b93fa5fffd618299b7704339dced507151
AtharvaPanegai <panegai.atharv@gmail.com>
2022-01-21 13:39:35 +0530

    Signup User and Cookie

--- controller/userController.js
+++ controller/userController.js
@@ -2,8 +2,21 @@
 
 const User = require("../models/user");
 const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
+const cookieToken = require("../utils/cookieToken");
 
 exports.signup = BigPromise(async (req, res, next) => {
-  //
-  res.send("This is test Signup ROute");
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return next(new CustomError("Fields are Missing", 400));
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  // these functions will be used again and again in resetPassword and more places
+  cookieToken(user,res);
 });
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 const { signup } = require("../controller/userController");
 
-router.route("/signup").get(signup);
+router.route("/signup").post(signup);
 
 module.exports = router;
