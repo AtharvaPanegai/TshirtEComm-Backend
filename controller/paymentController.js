ef5c77c7b702a3e57426111a37231cfeabb60153
AtharvaPanegai <panegai.atharv@gmail.com>
2022-02-09 14:46:19 +0530

    Creating a razorpay Payment Controller

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -17,7 +17,32 @@ exports.captureStripePayment = BigPromise(async (req, res, next) => {
   });
   res.status(200).json({
     success: true,
+    amount: req.body.amount,
     client_secret: paymentIntent.client_secret,
     payment_id: paymentIntent.id,
+  });
+});
+
+exports.sendRazorpayKey = BigPromise(async (req, res, next) => {
+  res.status(200).json({
+    razorPayPubKey: process.env.RAZORPAY_PUBKEY,
+  });
+});
+
+exports.captureRazorpayPayment = BigPromise(async (req, res, next) => {
+  var instance = new Razorpay({
+    key_id: process.env.RAZORPAY_PUBKEY,
+    key_secret: process.env.RAZORPAY_SECKEY,
+  });
+  instance.orders.create({
+    amount: req.body.amount,
+    currency: "INR",
+  });
+  const myOrder = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    amount: req.body.amount,
+    order: myOrder,
   });
 });
