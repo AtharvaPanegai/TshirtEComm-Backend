74b3d6a39bd2deb793899cd970baca7cc5e49084
AtharvaPanegai <panegai.atharv@gmail.com>
2022-02-10 06:17:56 +0530

    Creating getOne Order Controller and Route to it

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,7 +2,7 @@
 
 const Order = require("../models/order");
 const Product = require("../models/product");
-
+const CustomError = require("../utils/customError");
 const BigPromise = require("../middlewares/bigPromise");
 
 exports.createOrder = BigPromise(async (req, res, next) => {
@@ -24,6 +24,22 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     totalAmount,
     user: req.user._id,
   });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getOneOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return new (CustomError("No Order Found", 404))();
+  }
 
   res.status(200).json({
     success: true,
--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,12 @@
 /** @format */
 
 const express = require("express");
-const { createOrder } = require("../controller/orderController");
+const { createOrder, getOneOrder } = require("../controller/orderController");
 const router = express.Router();
 
 const { isLoggedIn, customRole } = require("../middlewares/userMiddleware");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
+router.route("/order/:id").get(isLoggedIn, getOneOrder);
 
 module.exports = router;
